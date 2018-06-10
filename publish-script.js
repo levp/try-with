882162beaf9f0089b1cbbdf8f272a2567290623f
levp <levi2p@gmail.com>
2018-06-10 19:50:34 +0300

    Fixed publish script not copying README into published directory.

--- publish-script.js
+++ publish-script.js
@@ -3,56 +3,85 @@
 // Imports
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 const child_process = require('child_process');
+
+const fsp = {
+	readFile: util.promisify(fs.readFile),
+	writeFile: util.promisify(fs.writeFile),
+};
 
 // Configuration
 const ROOT_DIR = __dirname;
 const DIST_DIR = path.join(ROOT_DIR, './dist/');
 const NPM_COMMAND = osIsWindows() ? 'npm.cmd' : 'npm';
 
+const PACKAGE_JSON_PATH = path.join(ROOT_DIR, './package.json');
+const README_PATH = path.join(ROOT_DIR, './README.md');
+
 ///////////////////////////////////////////////////////////
-// Publish
-///////////////////////////////////////////////////////////
 
-// Get package.json
-const originalPkg = require('./package.json');
+publishScript();
 
-// Remove the `private` option
-delete originalPkg.private;
-
-// Convert back to a formatted JSON string.
-const fixedPkg = JSON.stringify(originalPkg, null, 2);
-
-// Create fixed `package.json` in the dist directory.
-fs.writeFileSync(path.join(DIST_DIR, './package.json'), fixedPkg);
-
-console.log('Spawning build process . . .');
-const buildProcess = spawnBuildProcess();
-buildProcess.on('close', code => {
-	if (code !== 0) {
-		console.log(`ERROR! Build process could not complete, exited with error code ${code}.`);
-		return;
-	}
+async function publishScript() {
+	await runBuildProcess();
+	await Promise.all([
+		copyPackageJsonToDist(),
+		copyReadmeToDist(),
+	]);
 	console.log('Build process finished successfully, spawning publish process . . .');
+	await spawnPublishDistProcess();
+	console.log('Build publish finished successfully!');
+}
 
-	spawnPublishDistProcess();
-});
+async function copyPackageJsonToDist() {
+	// Get package.json contents
+	const packageJsonContents = await fsp.readFile(PACKAGE_JSON_PATH);
+	const originalPkg = JSON.parse(packageJsonContents.toString());
+	// Remove the `private` option
+	delete originalPkg.private;
+	// Convert back to a formatted JSON string.
+	const fixedPkg = JSON.stringify(originalPkg, null, 2);
+	// Create fixed `package.json` in the dist directory.
+	await fsp.writeFile(path.join(DIST_DIR, './package.json'), fixedPkg);
+}
 
-///////////////////////////////////////////////////////////
-// Spawning
-///////////////////////////////////////////////////////////
+async function copyReadmeToDist() {
+	const readmeContents = await fsp.readFile(README_PATH);
+	await fsp.writeFile(path.join(DIST_DIR, './README.md'), readmeContents);
+}
 
-function spawnBuildProcess() {
-	return child_process.spawn(NPM_COMMAND, ['run', 'build'], {
-		cwd: ROOT_DIR,
-		stdio: 'inherit',
+async function runBuildProcess() {
+	return new Promise((resolve, reject) => {
+		const buildProcess = child_process.spawn(NPM_COMMAND, ['run', 'build'], {
+			cwd: ROOT_DIR,
+			stdio: 'inherit',
+		});
+
+		buildProcess.on('close', code => {
+			if (code !== 0) {
+				reject(new Error(`ERROR! Build process could not complete, exited with error code ${code}.`));
+				return;
+			}
+			resolve();
+		});
 	});
 }
 
-function spawnPublishDistProcess() {
-	return child_process.spawn(NPM_COMMAND, ['publish'], {
-		cwd: DIST_DIR,
-		stdio: 'inherit',
+async function spawnPublishDistProcess() {
+	return new Promise((resolve, reject) => {
+		const publishProcess = child_process.spawn(NPM_COMMAND, ['publish'], {
+			cwd: DIST_DIR,
+			stdio: 'inherit',
+		});
+
+		publishProcess.on('close', code => {
+			if (code !== 0) {
+				reject(`ERROR! Publish process could not complete, exited with error code ${code}.`);
+				return;
+			}
+			resolve();
+		});
 	});
 }
 
